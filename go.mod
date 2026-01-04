6ba31fa441f912f388ab5f0b98a1ad4c990909f7
aldhipradana <aldhipradana@gmail.com>
2026-01-05 01:04:02 +0800

    feat: update database driver support and improve configuration handling

--- go.mod
+++ go.mod
@@ -9,6 +9,7 @@ require (
 )
 
 require (
+	filippo.io/edwards25519 v1.1.0 // indirect
 	github.com/bytedance/sonic v1.14.0 // indirect
 	github.com/bytedance/sonic/loader v0.3.0 // indirect
 	github.com/cloudwego/base64x v0.1.6 // indirect
@@ -17,8 +18,13 @@ require (
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.27.0 // indirect
+	github.com/go-sql-driver/mysql v1.8.1 // indirect
 	github.com/goccy/go-json v0.10.2 // indirect
 	github.com/goccy/go-yaml v1.18.0 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
+	github.com/jackc/pgx/v5 v5.6.0 // indirect
+	github.com/jackc/puddle/v2 v2.2.2 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/cpuid/v2 v2.3.0 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
@@ -46,5 +52,7 @@ require (
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	golang.org/x/text v0.32.0 // indirect
+	gorm.io/driver/mysql v1.6.0
+	gorm.io/driver/postgres v1.6.0
 	gorm.io/driver/sqlite v1.6.0
 )
--- main.go
+++ main.go
@@ -8,40 +8,47 @@ import (
 	"github.com/aldhipradana/warehouse-api/models"
 	"github.com/aldhipradana/warehouse-api/routes"
 	"github.com/gin-gonic/gin"
+	"gorm.io/driver/mysql"
+	"gorm.io/driver/postgres"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
 
 func main() {
-	// Load configuration
 	cfg, err := config.LoadConfig("config.toml")
 	if err != nil {
 		log.Fatalf("Failed to load config: %v", err)
 	}
 
-	// Set Gin mode based on debug setting
 	if !cfg.Server.Debug {
 		gin.SetMode(gin.ReleaseMode)
 	}
 
-	// Setup DB using config
-	db, err := gorm.Open(sqlite.Open(cfg.Database.GetDSN()), &gorm.Config{})
+	var dialector gorm.Dialector
+	dsn := cfg.Database.GetDSN()
+
+	switch cfg.Database.Driver {
+	case "mysql":
+		dialector = mysql.Open(dsn)
+	case "postgres":
+		dialector = postgres.Open(dsn)
+	case "sqlite":
+		dialector = sqlite.Open(dsn)
+	default:
+		log.Fatalf("Unsupported db driver: %s", cfg.Database.Driver)
+	}
+
+	db, err := gorm.Open(dialector, &gorm.Config{})
 	if err != nil {
 		log.Fatalf("Failed to connect to database: %v", err)
 	}
-	db.AutoMigrate(&models.Product{}, &models.User{})
 
-	// Initialize auth middleware with config
+	db.AutoMigrate(&models.Product{}, &models.User{})
 	middleware.InitAuth(cfg)
 
 	r := gin.Default()
-
-	// Use Action Logger Middleware
 	r.Use(middleware.ActionLogger())
-
-	// Register Routes
 	routes.RegisterRoutes(r, db)
 
-	// Run server with configured port
 	r.Run(cfg.Server.GetServerAddress())
 }
